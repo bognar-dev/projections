8fd00f9ec71bb959d296a91ab5d921536160286d
Niklas Bognar <94807001+nibog001@users.noreply.github.com>
2024-01-15 10:01:47 +0000

    Add new files and update dependencies

--- src/app/(components)/navbar.tsx
+++ src/app/(components)/navbar.tsx
@@ -1,32 +1,46 @@
-
+"use client"
 import Link from "next/link"
 import { JSX, SVGProps } from "react"
-
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu"
+import { useRouter, useSearchParams, usePathname,useParams } from "next/navigation"
 export default function Navbar() {
+  const params = useParams()
+  console.log(params)
   return (
-    <nav className="bg-gray-50 px-1 py-2 shadow-md">
-      <div className="container flex items-center justify-items-stretch content-between justify-between min-w-full">
-
+    <NavigationMenu className="bg-background px-1 py-2 shadow-md container flex items-center justify-items-stretch content-between justify-between min-w-full">
         <div className=" pl-2 flex items-center space-x-4">
-          <Link className="text-md font-semibold uppercase tracking-wide" href="#">
+          <NavigationMenuLink className="text-md font-semibold uppercase tracking-wide" href={`
+          
+          `}>
             Projections
-          </Link>
-          <Link className="text-md font-semibold uppercase tracking-wide" href="#">
+          </NavigationMenuLink>
+          <NavigationMenuLink className="text-md font-semibold uppercase tracking-wide" href="#">
             By Adela Novakova
-          </Link>
+          </NavigationMenuLink>
         </div>
 
         <div className="flex items-center space-x-4 pr-2 ">
           <Link className="text-md font-semibold uppercase tracking-wide" href="#">
             Portfolio
           </Link>
-          <Link className="text-md font-semibold uppercase tracking-wide" href="#">
-            Fall / Winter
+          <Link className="text-md font-semibold uppercase tracking-wide" href={
+            `?season=${params.season}`
+          }>
+            {params.season}
           </Link>
           <MenuIcon className="h-6 w-6" />
         </div>
-      </div>
-    </nav>
+      
+    </NavigationMenu>
   )
 }
 
