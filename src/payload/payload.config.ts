2a8d91f7ddb4ccb14ec5e1bc3a3d8df5fa0f9ff4
Niklas Bognar <94807001+nibog001@users.noreply.github.com>
2024-01-25 19:25:41 +0000

    Payload loaders missing

---
+++ src/payload/payload.config.ts
@@ -0,0 +1,65 @@
+import path from 'path';
+import { buildConfig } from 'payload/config';
+import { postgresAdapter } from '@payloadcms/db-postgres';
+import { slateEditor } from '@payloadcms/richtext-slate';
+import { webpackBundler } from '@payloadcms/bundler-webpack';
+
+export default buildConfig({
+  db: postgresAdapter({
+    // Postgres-specific arguments go here.
+    // `pool` is required.
+    pool: {
+      connectionString: process.env.DATABASE_URI,
+    }
+  }),
+  editor: slateEditor({}),
+  admin: {
+    bundler: webpackBundler(),
+    webpack(config) {
+      config.module = {
+        rules: [
+          { test: /\.js$/, include: /node_modules/, loader: 'license-loader' }
+        ]
+      };
+      return config;
+    }
+  },
+  collections: [
+    {
+      slug: 'pages',
+      fields: [
+        {
+          name: 'title',
+          type: 'text',
+          required: true,
+        },
+        {
+          name: 'content',
+          type: 'richText',
+          required: true,
+        },
+      ],
+    },
+  ],
+  globals: [
+    {
+      slug: 'header',
+      fields: [
+        {
+          name: 'nav',
+          type: 'array',
+          fields: [
+            {
+              name: 'page',
+              type: 'relationship',
+              relationTo: 'pages',
+            },
+          ],
+        },
+      ],
+    },
+  ],
+  typescript: {
+    outputFile: path.resolve(__dirname, '../payload-types.ts'),
+  },
+});
